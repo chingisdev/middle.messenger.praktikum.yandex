ce06442d4bb423ef9389a84e20ce5a1980172e56
Chingis <chingisdev@gmail.com>
2022-03-01 14:42:16 +0300

    fix bundler

--- src/components/Iterable/Iterable.ts
+++ src/components/Iterable/Iterable.ts
@@ -1,7 +1,7 @@
 import Block from '../../utils/Components/Block';
 import template from './template.hbs';
 
-export default class Iterable extends Block<{}> {
+export default class Iterable extends Block<any> {
   protected render(): DocumentFragment {
     return this.compile(template, { ...this.props });
   }
